1e350d01ec1c3d7561b9cf21fc94c8ff1b70a399
fzxt <fahadahmad41@hotmail.com>
2016-04-08 18:34:53 -0400

    fizzy buzzy buzz

---
+++ fzxt/fizzbuzz.java
@@ -0,0 +1,20 @@
+public class FizzBuzz{
+  public static void main(String[] args){
+
+    for(int i = 1 ; i <= 100 ; ++i){
+      String str = "";
+
+      if(i % ( 5 * 3) == 0){
+        str = "FizzBuzz";
+      } else if(i % 3 == 0){
+        str = "Fizz";
+      } else if(i % 5 == 0){
+        str = "Buzz";
+      } else {
+        str = Integer.toString(i);
+      }
+
+      System.out.println(str);
+    }
+  }
+}
